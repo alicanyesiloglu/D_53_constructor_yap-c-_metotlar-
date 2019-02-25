74c158a05399065e389e491a50e775976ce22188
alican <doc.alican@hotmail.com>
2019-02-26 00:23:51 +0300

    constructor yapıcı metotlar

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d_53_Constructor_yapıcı_metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+
+                string ad,soyad,sinif,ders;
+                Console.Write("ADI:");
+                ad = Console.ReadLine();
+
+                Console.Write("SOYAD:");
+                soyad = Console.ReadLine();
+
+                Console.Write("DERS:");
+                ders = Console.ReadLine();
+
+                Console.Write("SINIF:");
+                sinif = Console.ReadLine();
+
+                Console.WriteLine("/n/n/n");
+                ogrenci ogr = new ogrenci(ad,soyad,ders,sinif );
+                Console.Read();
+
+            }
+        }
+    }
+}
---
+++ ogrenci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d_53_Constructor_yapıcı_metotlar
+{
+    class ogrenci
+    {
+
+        public ogrenci(string ad,string soyad,string ders,string sinif)
+        {
+
+         
+            Console.WriteLine("Adi" + ad);
+            Console.WriteLine("Soyadi" + soyad);
+            Console.WriteLine("Ders" + ders);
+            Console.WriteLine("Sinif" + sinif);
+          
+                }
+
+
+    }
+}
